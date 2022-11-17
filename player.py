b16022838cdb111754782de756863ed23a88ef8c
John Lacalamita <johnlacalamita@Johns-MacBook-Air.local>
2022-11-17 17:57:20 -0500

    extract player stats from basketball reference

--- player.py
+++ player.py
@@ -4,23 +4,22 @@ import json
 
 class Player:
     points = 0
-    def __init__(self, id):
-        self.id = id
-
-    def stats(self):
-        client.regular_season_player_box_scores(
-        player_identifier=self.id, 
-        season_end_year=2023,
-        output_type=OutputType.JSON,
-        output_file_path="./stats.json")
+    name = ""
 
 
-cat = Player("westbru01")
-cat.stats()
+
+client.players_season_totals(
+    season_end_year=2023, 
+    output_type=OutputType.JSON, 
+    output_file_path="./stats.json"
+)
 with open('stats.json') as f:
    data = json.load(f)
-points = 0
+Players = []
 for i in range(len(data)):
-    cat.points += data[i]['points_scored']
-cat.points = points/len(data)
-print(cat.points/len(data))
+    cat = Player
+    cat.name = data[i]['name']
+    cat.points = data[i]['points'] / len(data)
+    Players.append(cat)
+print(Players[0].points)
+print(Players[0].name)
